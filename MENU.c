2fafa6be0a0ef68ccf94c4efede392466ddb137b
Mateus C <129126812+MateusCerqueiraG@users.noreply.github.com>
2023-11-22 14:06:24 -0300

    MENU
    commit 1, menu.

---
+++ MENU.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main () {
+
+    printf("        --------------------------\n");
+    printf("        |          MENU          |\n");
+    printf("        --------------------------\n");
+
+    printf("------------------------------------------\n");
+    printf("|     1- Adicionar Nova Musica.          |\n");
+    printf("|     2- Favoritar Musica.               |\n");
+    printf("|     3- Escolher Musica Aleatoria.      |\n");
+    printf("|     4- Remover Musica.                 |\n");
+    printf("|     5- Mostrar Musicas Da Playlist.    |\n");
+    printf("|     6- Editar Musica Da Playlist.      |\n");
+    printf("|     7- Buscar Uma Musica.              |\n");
+    printf("|     8- Baixar Playlist.                |\n");
+    printf("------------------------------------------\n");
+
+    return 0;
+}
