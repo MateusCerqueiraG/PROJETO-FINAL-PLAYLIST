c52e78636ec1225db92c9caa9cb44dbe41f37648
ViniciusAdson <146032101+ViniciusAdson@users.noreply.github.com>
2023-12-05 22:20:31 -0300

    Favoritar.c

--- Favoritar.c
+++ Favoritar.c
@@ -1,49 +1,54 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#define MAX_MUSICAS 100
 
-typedef struct Music {
-    char title[50];
-    int favorite;
-} Music;
+struct Musica {
+    char nome[50];
+    char artista[50];
+    int favorita;
+};
 
-void addMusic(Music *music, int index);
-void listMusic(Music *music, int total);
-void removeMusic(Music *music, int index, int total);
+struct Playlist {
+    char nome[50];
+    struct Musica musicas[MAX_MUSICAS];
+    int totalMusicas;
+};
+
+void adicionarMusica(struct Playlist *playlist, const char *nome, const char *artista) {
+    if (playlist->totalMusicas < MAX_MUSICAS) {
+        struct Musica novaMusica;
+        strcpy(novaMusica.nome, nome);
+        strcpy(novaMusica.artista, artista);
+        novaMusica.favorita = 0; // Inicialmente, a música não é favorita
+
+        playlist->musicas[playlist->totalMusicas] = novaMusica;
+        playlist->totalMusicas++;
+        printf("Música '%s' adicionada à playlist '%s'.\n", nome, playlist->nome);
+    } else {
+        printf("A playlist atingiu o número máximo de músicas.\n");
+    }
+}
+
+void favoritarMusica(struct Playlist *playlist, int indice) {
+	if(indice >= 0 && indice < playlist ->totalMusicas){
+		playlist->musicas[indice].favorita = 1;
+		
+		printf("Música '%s' favoritada na playlist '%s'.\n", playlist->musicas[indice].nome, playlist->nome);
+	}else{
+		printf("Música não encontrada na playlist.\n");
+	}
+}
 
 int main() {
-    Music music[50];
-    int total = 0;
-    int option;
+    struct Playlist minhaPlaylist;
+    strcpy(minhaPlaylist.nome, "Minha Playlist");
+    minhaPlaylist.totalMusicas = 0;
 
-    do {
-        printf("\nMenu de Musicas:\n");
-        printf("1. Adicionar musica\n");
-        printf("2. Listar musicas\n");
-        printf("3. Remover musica\n");
-        printf("4. Sair\n");
-        printf("Digite sua opcao: ");
-        scanf("%d", &option);
+    adicionarMusica(&minhaPlaylist, "Musica1", "Artista1");
+    adicionarMusica(&minhaPlaylist, "Musica2", "Artista2");
 
-        switch (option) {
-            case 1:
-                addMusic(music, total);
-                total++;
-                break;
-            case 2:
-                listMusic(music, total);
-                break;
-            case 3:
-                removeMusic(music, total - 1, total);
-                total--;
-                break;
-            case 4:
-                printf("Saindo...\n");
-                break;
-            default:
-                printf("Opcao invalida! Tente novamente.\n");
-        }
-    } while (option != 4);
+    favoritarMusica(&minhaPlaylist, 0);
+    favoritarMusica(&minhaPlaylist, 1);
 
     return 0;
 }
